1e350c83ceb784264424a3154571345ad2fdef36
Kiki Pratiwi <kiki.pratiwi.tif17@polban.ac.id>
2018-12-16 16:24:10 +0700

    change db name config

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_DATABASE', 'proyek');
+    define('DB_DATABASE', 'db_sertify');
     $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
