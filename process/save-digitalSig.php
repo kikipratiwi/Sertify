13e667396fce60f0294220906aca18402e9827fb
Kiki Pratiwi <kiki.pratiwi.tif17@polban.ac.id>
2018-12-16 15:55:44 +0700

    edit gensig file

--- process/save-digitalSig.php
+++ process/save-digitalSig.php
@@ -1,26 +1,6 @@
 <?php
-    include "connection.php";
-    date_default_timezone_set("Asia/Jakarta");
 
-    $id = $_POST['no'];
-    $status = $_POST['status'];
-    $verified_at = DateTime())->format('Y-m-d H:i:s');
-
-    if ($status == "R") {
-        $digital_signature = "-";
-    } else {
-        $digital_signature = $_POST["digital_signature"];
-    }
-
-    if (!exec_update()) {
-        echo "<h4> Update Failed </h4>";
-    }
-
-    function exec_update() {
-        $query = "UPDATE certificates SET digital_signature = '$digital_signature',  status= '$status' , verified_at= '$verified_at' WHERE  id = '$id'";
-        $execute = mysqli_query($conn,$query);
-
-        return $execute;
-    }
-
+echo '<script language="javascript">';
+                echo 'alert("Data berhasil diupload")';
+                echo '</script>';
 } 
